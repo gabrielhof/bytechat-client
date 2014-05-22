d0e3e486b4fb4669e6b5ecbcbe4965ccfd046b09
Gabriel Hoff <gabriel@i360tecnologia.com.br>
2014-05-22 01:39:50 -0300

    Arquivos

--- src/main/java/br/feevale/bytechat/client/gui/ChatFrame.java
+++ src/main/java/br/feevale/bytechat/client/gui/ChatFrame.java
@@ -3,15 +3,24 @@ package br.feevale.bytechat.client.gui;
 import java.awt.Dimension;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.DefaultListModel;
+import javax.swing.ListSelectionModel;
 
 import br.feevale.bytechat.builder.MessageBuilder;
 import br.feevale.bytechat.client.ChatClient;
 import br.feevale.bytechat.client.listener.ShutdownWindowListener;
 import br.feevale.bytechat.client.listener.SwingSessionListener;
 import br.feevale.bytechat.exception.ClientException;
+import br.feevale.bytechat.packet.Message;
+import br.feevale.bytechat.packet.UserList;
 import br.feevale.bytechat.util.FileUtils;
+import br.feevale.bytechat.util.User;
 
 import com.alee.laf.WebLookAndFeel;
+import com.alee.laf.list.WebList;
 import com.alee.laf.rootpane.WebFrame;
 import com.alee.laf.scroll.WebScrollPane;
 import com.alee.laf.splitpane.WebSplitPane;
@@ -22,7 +31,10 @@ public class ChatFrame extends WebFrame {
 
 	private static final long serialVersionUID = -4127225470877660106L;
 
-	private static final Dimension DEFAULT_DIMENSION = new Dimension(300, 500);
+	private static final Dimension DEFAULT_DIMENSION = new Dimension(500, 500);
+	
+	private DefaultListModel<User> userListModel = new DefaultListModel<User>();
+	private WebList userWebList;
 	
 	private String htmlTemplate;
 	private StringBuilder consoleBuilder = new StringBuilder();
@@ -52,6 +64,7 @@ public class ChatFrame extends WebFrame {
 	private void setupChat() {
 		try {
 			client.getSession().addListener(new SwingSessionListener(this));
+			client.getSession().send(new UserList());
 			Runtime.getRuntime().addShutdownHook(new ChatConnectionChatDown());
 		} catch (Exception e) {
 			throw new RuntimeException(e);
@@ -59,8 +72,11 @@ public class ChatFrame extends WebFrame {
 	}
 
 	protected void createComponents() {
-		htmlTemplate = FileUtils.getContents("template.html", true);
+		userWebList = new WebList(userListModel);
+		userWebList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		userWebList.addMouseListener(new WebListMouseListener());
 		
+		htmlTemplate = FileUtils.getContents("template.html", true);
 		console = new WebEditorPane("text/html", String.format(htmlTemplate, ""));
 		console.setEditable(false);
 		
@@ -71,11 +87,17 @@ public class ChatFrame extends WebFrame {
 	}
 
 	protected void buildLayout() {
+		WebScrollPane userListPane = new WebScrollPane(userWebList);
+		
 		WebScrollPane consoleScroll = new WebScrollPane(console);
 		WebScrollPane messageScroll = new WebScrollPane(messageField);
 		
-		WebSplitPane panel = new WebSplitPane(WebSplitPane.VERTICAL_SPLIT, consoleScroll, messageScroll);
-		panel.setDividerLocation(DEFAULT_DIMENSION.height - 100);
+		WebSplitPane chatPanel = new WebSplitPane(WebSplitPane.VERTICAL_SPLIT, consoleScroll, messageScroll);
+		chatPanel.setDividerLocation(DEFAULT_DIMENSION.height - 100);
+		
+		WebSplitPane panel = new WebSplitPane(WebSplitPane.HORIZONTAL_SPLIT, userListPane, chatPanel);
+		panel.setDividerLocation(150);
+		
 		add(panel);
 		
 		setSize(DEFAULT_DIMENSION);
@@ -88,6 +110,10 @@ public class ChatFrame extends WebFrame {
 		console.setCaretPosition(console.getDocument().getLength());
 	}
 	
+	public DefaultListModel<User> getUserModel() {
+		return userListModel;
+	}
+	
 	class MessageSenderKeyListener extends KeyAdapter {
 		
 		@Override
@@ -98,13 +124,56 @@ public class ChatFrame extends WebFrame {
 						e.consume();
 						
 						String message = messageField.getText();
-						client.getSession().send(MessageBuilder.create().from(client.getUser()).withMessage(message).getMessage());
+						Message m = parseMessage(message);
+						client.getSession().send(m);
 						
-						appendMessage(String.format("<p style='color: #75BA78;'>Você disse: <span style='color: black;'>%s</span></p>", message));
+						if (m.isPrivate()) {
+							appendMessage(String.format("<p style='color: #75BA78;'>Você disse para <span style='color: #D4BB00;'>@</span>%s: <span style='color: black;'>%s</span></p>", m.getRecipients().get(0).getName(), m.getMessage()));
+						} else {
+							appendMessage(String.format("<p style='color: #75BA78;'>Você disse: <span style='color: black;'>%s</span></p>", message));
+						}
 						messageField.setText("");
 					} catch (Exception e2) {
 						e2.printStackTrace();
 					}
+				}
+			}
+		}
+		
+		private Message parseMessage(String message) {
+			MessageBuilder builder = MessageBuilder.create();
+			builder.from(client.getUser());
+			
+			message = message.trim();
+			if (message.startsWith("@")) {
+				int spacePos = message.indexOf(" ");
+				builder.to(new User(spacePos > -1 ? message.substring(1, spacePos) : message));
+				
+				message = spacePos > -1 ? message.substring(spacePos+1) : "";
+			}
+			
+			builder.withMessage(message);
+			
+			return builder.getMessage();
+		}
+		
+	}
+	
+	class WebListMouseListener extends MouseAdapter {
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (e.getClickCount() == 2) {
+				User user = (User) userWebList.getSelectedValue();
+				if (user != null) {
+					String currentMessage = messageField.getText();
+					if (currentMessage.startsWith("@")) {
+						int spacePos = currentMessage.indexOf(" ");
+						currentMessage = (spacePos > -1 ? currentMessage.substring(spacePos+1) : "");
+					}
+					
+					currentMessage = String.format("@%s %s", user.getName(), currentMessage);
+					messageField.setText(currentMessage);
 				}
 			}
 		}
--- src/main/java/br/feevale/bytechat/client/listener/SwingSessionListener.java
+++ src/main/java/br/feevale/bytechat/client/listener/SwingSessionListener.java
@@ -1,21 +1,131 @@
 package br.feevale.bytechat.client.listener;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import br.feevale.bytechat.client.gui.ChatFrame;
 import br.feevale.bytechat.listener.AbstractSessionListener;
+import br.feevale.bytechat.packet.Bind;
+import br.feevale.bytechat.packet.File;
 import br.feevale.bytechat.packet.Message;
+import br.feevale.bytechat.packet.Unbind;
+import br.feevale.bytechat.packet.UserList;
+import br.feevale.bytechat.util.FileUtils;
 import br.feevale.bytechat.util.Session;
+import br.feevale.bytechat.util.User;
+
+import com.alee.extended.panel.CenterPanel;
+import com.alee.extended.window.WebProgressDialog;
+import com.alee.laf.label.WebLabel;
+import com.alee.laf.optionpane.WebOptionPane;
+import com.alee.managers.notification.NotificationIcon;
+import com.alee.managers.notification.NotificationManager;
+import com.alee.managers.notification.WebNotificationPopup;
 
 public class SwingSessionListener extends AbstractSessionListener {
 
 	private ChatFrame chatFrame;
+	
+	private Map<String, FileDialog> files = new HashMap<String, FileDialog>();
 	
 	public SwingSessionListener(ChatFrame chatFrame) {
 		this.chatFrame = chatFrame;
 	}
 	
 	@Override
+	public void bindReceived(Session source, final Bind bind) {
+		showInfo(String.format("@%s acabou de entrar", bind.getUser().getName()));
+		chatFrame.getUserModel().addElement(bind.getUser());
+	}
+	
+	@Override
+	public void unbindReceived(Session source, Unbind unbind) {
+		showInfo(String.format("@%s acabou de sair", unbind.getUser().getName()));
+		chatFrame.getUserModel().removeElement(unbind.getUser());
+	}
+	
+	@Override
+	public void userListReceived(Session source, UserList userList) {
+		chatFrame.getUserModel().removeAllElements();
+		
+		for (User user : userList.getUsers()) {
+			if (!chatFrame.getUserModel().contains(user)) {
+				chatFrame.getUserModel().addElement(user);
+			}
+		}
+	}
+	
+	@Override
+	public void fileReceived(Session source, File file) {
+		try {
+			FileDialog dialog = null;
+			
+			if (!files.containsKey(file.getFileId())) {
+				WebProgressDialog progress = new WebProgressDialog("Baixando arquivo");
+				progress.setMaximum(file.getTotalParts());
+				
+				java.io.File f = java.io.File.createTempFile("chat_temp_", "_" + file.getName());
+				dialog = new FileDialog(f,progress);
+				files.put(file.getFileId(), dialog);
+			} else {
+				dialog = files.get(file.getFileId());
+			}
+			
+			OutputStream output = new FileOutputStream(dialog.getFile(), true);
+			output.write(file.getContents());
+			output.flush();
+			output.close();
+			
+			dialog.getProgress().setProgress(file.getPart());
+			if (file.getPart() == file.getTotalParts()) {
+				files.remove(file.getFileId());
+				
+				java.io.File f = FileUtils.moveToUserDir(dialog.getFile(), file.getName());
+				dialog.getProgress().dispose();
+				WebOptionPane.showMessageDialog(null, String.format("Arquivo %s recebido e salvo em %s", file.getName(), f.getAbsolutePath()));
+			}
+		} catch (IOException e) {
+		}
+	}
+	
+	@Override
 	public void messageReceived(Session source, Message message) {
-		chatFrame.appendMessage(String.format("<p style='color: #3B5998;'>%s disse: <span style='color: black;'>%s</span></p>", message.getOriginator().getName(), message.getMessage()));
+		if (!message.isPrivate()) {
+			chatFrame.appendMessage(String.format("<p style='color: #3B5998;'><span style='color: #D4BB00;'>@</span>%s disse: <span style='color: black;'>%s</span></p>", message.getOriginator().getName(), message.getMessage()));
+		} else {
+			chatFrame.appendMessage(String.format("<p style='color: red;'>(mensagem privada) @%s disse: %s</p>", message.getOriginator().getName(), message.getMessage()));
+		}
+	}
+	
+	private void showInfo(String message) {
+		WebNotificationPopup popup = new WebNotificationPopup();
+		popup.setIcon(NotificationIcon.information);
+		popup.setDisplayTime(2000);
+		popup.setContent(new CenterPanel(new WebLabel(message)));
+		
+		NotificationManager.showNotification(chatFrame, popup);
+	}
+	
+	class FileDialog {
+		
+		private java.io.File file;
+		private WebProgressDialog progress;
+		
+		public FileDialog(java.io.File file, WebProgressDialog progress) {
+			this.file = file;
+			this.progress = progress;
+		}
+		
+		public java.io.File getFile() {
+			return file;
+		}
+
+		public WebProgressDialog getProgress() {
+			return progress;
+		}
 	}
 	
 }
